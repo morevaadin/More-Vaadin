b4df11249b0bff705993175b21cb84b0cc66fb75
Nicolas Frankel <nicolas@frankel.ch>
2012-04-22 11:20:00 +0200

    Shortened the Scala code when possible

--- vaadin-scala-example/src/main/scala/com/morevaadin/vaadin/scala/CompositeFieldButton.scala
+++ vaadin-scala-example/src/main/scala/com/morevaadin/vaadin/scala/CompositeFieldButton.scala
@@ -23,15 +23,15 @@ class CompositeFieldButton() extends CustomComponent {
 
   val layout = new HorizontalLayout(spacing = true, margin = true) {
 
-    val button = add(new Button(caption = "Hello", action = _ => displayMessage()))
+    val button = add(new Button("Hello", _ => displayMessage))
 
     val field = add(new TextField(value = "world!"))
   }
 
   setCompositionRoot(layout)
 
-  def displayMessage(): Unit = {
+  def displayMessage() {
 
-    getWindow().showNotification(layout.button.getCaption() + " " + layout.field.getValue().asInstanceOf[String])
+    getWindow.showNotification(layout.button.getCaption + " " + layout.field.getValue)
   }
 }
